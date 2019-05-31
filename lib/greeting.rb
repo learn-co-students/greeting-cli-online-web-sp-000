650576ee04755bfa6326f6e6afe29ebac232e0a2
Chris Cheong <chrisxcheong@gmail.com>
2019-05-31 05:58:17 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,5 @@
 # code the #greeting method here!
-def greeting(name.chomp)
-  puts "Hello #{name}. It's nice to meet you."
+def greeting(name)
+  puts "Hello #{name.chomp.strip}. It's nice to meet you."
 
 end
