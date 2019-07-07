8b5823081c2ec3aac6f79a85c3ea914e53992378
David Strauch <dmaster18@gmail.com>
2019-07-07 21:14:34 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,8 +1,4 @@
-# code the #greeting method here!
-puts "Hi! Welcome to the wonderful world of Ruby programming."
-puts "Please enter your name so that we can greet you more personally:"
-name = gets.strip
+
 def greeting(name)
   puts "Hello #{name}. It's nice to meet you."
 end
-greeting(name)
