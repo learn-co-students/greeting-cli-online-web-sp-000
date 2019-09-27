df3389b97ecee917e982bb1d2193f5402d588a69
Paul Marshall <pmarshal33@gmail.com>
2019-09-27 21:21:04 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,7 @@
 # code the #greeting method here!
-
+require 'pry'
 def greeting(name)
-    puts "Hello #{name}. It's nice to meet you."
+  #binding.pry
+    puts "Hello #{name.chomp.strip}. It's nice to meet you."
 end
 
